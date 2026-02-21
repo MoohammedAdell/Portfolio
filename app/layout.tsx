8947bfdcefff29bc68d0a01b49de4ce444901395
mohamed adel <mohammedadell496@gmail.com>
2026-02-21 13:59:10 +0200

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
     siteName: "MADEL Portfolio",
     images: [
       {
-        url: "/og-image.jpeg",
+        url: "/og-image.png",
         width: 1200,
         height: 630,
         alt: "MADEL Portfolio Preview",
