c1870f77477b3ba665ce4c0aaa018416ad2abbba
mohamed adel <mohammedadell496@gmail.com>
2026-03-12 14:46:00 +0200

    update

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -101,6 +101,7 @@ export function HeroSection() {
               className="text-5xl sm:text-8xl md:text-9xl font-black tracking-[-0.04em] leading-[0.9] whitespace-nowrap text-white drop-shadow-lg"
             >
               Design <span className="text-white/20">Driven</span>
+              
             </motion.h1>
 
             <motion.h1
